c571929cdb10937bd50b299fcbb94b1aee6d6f81
Carl Scheffler <carl@siyavula.com>
2014-01-15 15:24:24 +0200

    Another attempt at UUIDs in JSON strings.

--- monassis/syncserver/core.py
+++ monassis/syncserver/core.py
@@ -100,11 +100,32 @@ def hash_hash_structure(struct):
     return hashlib.md5(repr(sorted([((x.encode('utf-8') if isinstance(x, basestring) else x), sorted([(y.encode('utf-8') if isinstance(y, basestring) else tuple([entry.encode('utf-8') if isinstance(entry, basestring) else entry for entry in y]) if isinstance(y, tuple) else y, z.encode('utf-8') if isinstance(z, basestring) else z) for y, z in subDict.items()])) for x, subDict in struct.items()]))).hexdigest()
 
 
+def struct_to_json(struct):
+    if isinstance(struct, dict):
+        raise TypeError, "Can not automatically convert dict"
+    elif isinstance(struct, list) or isinstance(struct, tuple):
+        return [struct_to_json(x) for x in struct]
+    elif isinstance(struct, UUID):
+        return repr(struct)
+    else:
+        return struct
+
+
+def json_to_struct(json):
+    if isinstance(struct, list):
+        return [json_to_struct(x) for x in struct]
+    elif isinstance(struct, basestring) and (struct[:5] == 'UUID('):
+        return eval(struct)
+    else:
+        return struct
+
+
 def actions_to_json(actions):
     # Modifies in place and returns the input list
     for sectionName, sectionData in actions.iteritems():
         for action in 'insert', 'update':
-            sectionData[action] = [(key, tuple([repr(x) if isinstance(x, UUID) else x for x in values])) for key, values in sectionData[action].iteritems()]
+            sectionData[action] = struct_to_json(sectionData[action].items())
+        sectionData['delete'] = struct_to_json(sectionData['delete'])
     return actions
 
 
@@ -112,6 +133,6 @@ def actions_from_json(actions):
     # Modifies in place and returns the input list
     for sectionName, sectionData in actions.iteritems():
         for action in 'insert', 'update':
-            sectionData[action] = dict([(tuple(key), tuple([eval(x) if x[:5] == 'UUID(' else x for x in values])) for key, values in sectionData[action]])
-        sectionData['delete'] = [tuple(key) for key in sectionData['delete']]
+            sectionData[action] = dict([(tuple(key), value) for key, value in json_to_struct(sectionData[action])])
+        sectionData['delete'] = [tuple(key) for key in json_to_struct(sectionData['delete'])]
     return actions
