2fbe054da3ab3b0d752c494e086b2b4f9492ced4
Carl Scheffler <carl@siyavula.com>
2014-02-11 12:28:59 +0200

    Improvements to compute_database_hash_hash.

--- scripts/compute_database_hash_hash.py
+++ scripts/compute_database_hash_hash.py
@@ -10,19 +10,21 @@ if __name__ == '__main__':
     hash_hash = md5()
     for dbmodel in [templatedb, bookdb, userdb, historydb]:
         database = dbmodel.db
-        for table_name in dbmodel.tables:
+        for table_name in sorted(dbmodel.tables.keys()):
             if table_name == 'record_hashes':
                 continue
-            print table_name
             table = dbmodel.tables[table_name]
-            select = sqlalchemy.sql.select([table])
+
+            select = sqlalchemy.sql.select([table]).limit(1)
             result = database.execute(select)
-            start = True
+            columns = result.fetchone().keys()
+            columns.sort()
+            columns = [table.c[column] for column in columns]
+
+            select = sqlalchemy.sql.select([table]).order_by(*columns)
+            result = database.execute(select)
             for row in result:
-                if start:
-                    columns = sorted(row.keys())
-                    start = False
-                for column in columns:
-                    hash_hash.update(repr(row[column]))
+                hash_hash.update(repr(tuple(row)))
             result.close()
+            print table_name, hash_hash.hexdigest()
     print hash_hash.hexdigest()
