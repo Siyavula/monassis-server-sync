a5ad4772e30b009a1a1be4c4e56918e0725f050f
Carl Scheffler <carl@siyavula.com>
2014-05-22 12:37:51 +0200

    Updated for use with database v1.6.0.

--- syncserver/record_database.py
+++ syncserver/record_database.py
@@ -134,29 +134,8 @@ Required methods:
 '''
 
 __DATABASE_REGISTRY = {
-    'bookdb': {
-        'module': 'monassis.books.dbmodel',
-        'version': 'DB_VERSION',
-        'database': 'db',
-        'tables': 'tables',
-        'hash_table': 'record_hashes',
-    },
-    'historydb': {
-        'module': 'monassis.historyservice.dbmodel',
-        'version': 'DB_VERSION',
-        'database': 'db',
-        'tables': 'tables',
-        'hash_table': 'record_hashes',
-    },
-    'templatedb': {
-        'module': 'monassis.qnxmlservice.dbmodel',
-        'version': 'DB_VERSION',
-        'database': 'db',
-        'tables': 'tables',
-        'hash_table': 'record_hashes',
-    },
-    'userdb': {
-        'module': 'monassis.usermanagement.dbmodel',
+    'monassisdb': {
+        'module': 'monassis.database',
         'version': 'DB_VERSION',
         'database': 'db',
         'tables': 'tables',
