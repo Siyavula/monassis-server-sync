f80de0b03b5f9f18ef3c78d0e049c53cc124bda4
Carl Scheffler <carl@siyavula.com>
2014-10-22 10:35:37 +0200

    Created network failure tests.

---
+++ tests-network/1_create_server_database.py
@@ -0,0 +1,20 @@
+import os, sys
+test_name = sys.argv[1]
+try:
+    with open(os.path.join(test_name, '1_create_server_database.py'), 'rt') as fp:
+        data = eval(fp.read())
+except IOError:
+    data = {}
+
+import ConfigParser
+parser = ConfigParser.SafeConfigParser()
+parser.read('server/database.cfg')
+db_uri = parser.get('databases', 'nosetests')
+
+from syncserver.tests import dbmodel
+dbmodel.load_from_uri(db_uri)
+for table in 'records', 'record_hashes':
+    dbmodel.tables[table].delete().execute()
+for table, rows in data.iteritems():
+    for row in rows:
+        dbmodel.tables[table].insert().values(**row).execute()
---
+++ tests-network/test_slave_master_sync/5_server_database.py
@@ -0,0 +1,26 @@
+{
+    'records': [
+        {'id': 1, 'column1': 'aa', 'column2': 'aa', 'column3': 'aa'},
+        {'id': 2, 'column1': 'bb', 'column2': 'bb', 'column3': 'bb'},
+        {'id': 4, 'column1': 'dd', 'column2': 'dd', 'column3': 'dd'},
+        {'id': 5, 'column1': 'ee', 'column2': 'ee', 'column3': 'ee'},
+        {'id': 6, 'column1': 'ff', 'column2': 'ff', 'column3': 'ff'},
+        {'id': 10, 'column1': 'j', 'column2': 'j', 'column3': 'j'},
+        {'id': 11, 'column1': 'k', 'column2': 'k', 'column3': 'k'},
+        {'id': 12, 'column1': 'l', 'column2': 'l', 'column3': 'l'},
+        {'id': 13, 'column1': 'mm', 'column2': 'mm', 'column3': 'mm'},
+        {'id': 14, 'column1': 'nn', 'column2': 'nn', 'column3': 'nn'},
+    ],
+    'record_hashes': [
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '1,', 'record_hash': '1a57b511abde26fb329449bd68a66fca'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '2,', 'record_hash': '125d5ccb56b2ce3b4a55734ef4a0fd5e'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '4,', 'record_hash': '2f019ad1aa9818c34561cff03685b4d2'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '5,', 'record_hash': '2ca2c44405b4822353fb1e7bd2c8cea2'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '6,', 'record_hash': '895dea6a73cc5dd4515b65449d8ae78e'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '10,', 'record_hash': 'ddcf2d042fb12d69a6aa025091863079'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '11,', 'record_hash': '9adfa122c15cd3ea743eb27eab6df352'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '12,', 'record_hash': 'f3461235e0adbf30cd2cee1366ed6c82'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '13,', 'record_hash': '50a8b247b6d363935573e617541b0fc7'},
+        {'sync_name': '__test__', 'section_name': 'records', 'record_id': '14,', 'record_hash': '1f614ed8473bfb86b36fb506a917b9f6'},
+    ],
+}
