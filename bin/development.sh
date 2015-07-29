4be6a6f8f6678b05e5326a5a415aa9a17d598621
Rich Cochrane <screamer42@gmail.com>
2015-07-29 16:16:06 +0200

    Remove unnecessary bin files

--- bin/development.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP_HOME=/home/carl/work/siyavula/code/monassis-server-sync
-
-cd $APP_HOME
-source env/bin/activate
-
-exec pserve production.ini 2>&1
--- bin/production.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP_HOME=$HOME/monassis-server-sync
-
-cd $APP_HOME
-source env/bin/activate
-
-exec pserve production.ini 2>&1
